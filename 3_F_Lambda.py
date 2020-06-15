6689b272df4cd2f227b7440f64a50d951ffb4776
jc <jcbb1995@hotmail.com>
2020-06-14 19:41:14 -0500

    Se modifico el archivo 3_F_Lambda

--- 3_F_Lambda.py
+++ 3_F_Lambda.py
@@ -2,7 +2,11 @@
 
 destacar_valor = lambda comision:"¡{}!$".format(comision)
 
+nuevo_formato = lambda salario: f"{salario}$$"
+
 
 comision_Ana = 15585
+salario_Enrique = 12000
 
 print(destacar_valor(comision_Ana))
+print(nuevo_formato(salario_Enrique))
